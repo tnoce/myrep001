3f9901047e3c6277708615990855c9a51ab751b8
tnoce <45219384+tnoce@users.noreply.github.com>
2019-04-02 17:27:43 +0900

    Update ec2.tf

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -5,19 +5,19 @@ variable "aws_region" {
 }
 variable "aws_zone" {
       default = "ap-northeast-1c"
-    }
+}
 
-    provider "aws" {
-        access_key = "${var.aws_access_key}"
-        secret_key = "${var.aws_secret_key}"
-        region     = "${var.aws_region}"
-    }
+provider "aws" {
+      access_key = "${var.aws_access_key}"
+      secret_key = "${var.aws_secret_key}"
+      region     = "${var.aws_region}"
+}
 
-    resource "aws_instance" "web1" {
-        ami           = "ami-f80e0596"
-        instance_type = "t2.micro"
-        monitoring    = true
-        tags {
-            Name = "web1"
-        }
-    }
+resource "aws_instance" "web1" {
+      ami           = "ami-f80e0596"
+      instance_type = "t2.micro"
+      monitoring    = true
+      tags {
+          Name = "web1"
+      }
+}

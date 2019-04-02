71e1600b15625f85f8ad69ab89eaebe3ecf09afb
長岡周 <volkjajaja@gmail.com>
2019-04-02 10:53:47 +0900

    edit rds.tf

--- rds.tf
+++ rds.tf
@@ -6,7 +6,7 @@ variable "database_user_name"{}
 variable "database_name"{}
 variable "database_character"{}
 
-provider "alicloud"{
+provider "alicloud" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
     region     = "${var.region}"
@@ -25,7 +25,7 @@ resource "alicloud_vswitch" "default"{
 resource "alicloud_db_instance" "default"{
     engine              =   "MySQL"
     engine_version      =   "5.7"
-    db_instance_class   =   "rds.mysql.t1.small"
-    db_instance_storage =   "10"
+    instance_type   =   "rds.mysql.t1.small"
+    instance_storage =   "40"
     vswitch_id          =   "${alicloud_vpc.default.id}"
 }

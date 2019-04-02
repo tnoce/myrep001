78ffcd89d7beb06166d1979d4146ec14b31ff3b7
tnoce <45219384+tnoce@users.noreply.github.com>
2019-04-02 12:02:29 +0900

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -7,25 +7,27 @@ variable "database_name"{}
 variable "database_character"{}
 
 provider "alicloud" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    region     = "${var.region}"
+    access_key          =   "${var.access_key}"
+    secret_key          =   "${var.secret_key}"
+    region              =   "${var.region}"
 }
 
 resource "alicloud_vpc" "default"{
-    name        =   "vpc-rds"
-    cidr_block  =   "172.16.0.0/16"
+    name                =   "vpc-rds"
+    cidr_block          =   "172.16.0.0/16"
 }
 
 resource "alicloud_vswitch" "default"{
-    vpc_id      =   "${alicloud_vpc.default.id}"
-    cidr_block  =   "172.16.0.0/24"
+    name                =   "vswitch-rds"
+    vpc_id              =   "${alicloud_vpc.default.id}"
+    cidr_block          =   "172.16.0.0/24"
+    availability_zone   =   "${var.zone}"
 }
 
 resource "alicloud_db_instance" "default"{
     engine              =   "MySQL"
     engine_version      =   "5.7"
-    instance_type   =   "rds.mysql.t1.small"
-    instance_storage =   "40"
-    vswitch_id          =   "${alicloud_vpc.default.id}"
+    instance_type       =   "rds.mysql.t1.small"
+    instance_storage    =   "5"
+    vswitch_id          =   "${alicloud_vswitch.default.id}"
 }
